09d56c84567f4f5f28d7bc5a0288d02aedb0fd40
Ravindra <ravindrababu1220@gmail.com>
2018-02-18 20:27:15 -0600

    My first change

--- src/main/java/com/deneverairport/baggage/model/Edge.java
+++ src/main/java/com/deneverairport/baggage/model/Edge.java
@@ -5,14 +5,17 @@ public class Edge {
 	private final Vertex source;
 	private final Vertex destination;
 	private final int weight;
+	public int ravindra;
 
 	public Edge(String id, Vertex source, Vertex destination, int weight) {
 		this.id = id;
 		this.source = source;
 		this.destination = destination;
 		this.weight = weight;
-	}
 
+	}
+		//I have commented this to make sure my local git tracks the changes
+		// Can I?
 	public String getId() {
 		return id;
 	}
